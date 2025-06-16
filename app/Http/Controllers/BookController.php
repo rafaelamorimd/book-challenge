eba0a02e9c22ed27968eee1cde640d7ab91f78a4
Rafael Amorim <thiago.rafael92@gmail.com>
2025-06-16 17:43:01 -0300

    Update BookController.php

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -76,6 +76,7 @@ class BookController extends Controller
 
     public function edit(Book $book): InertiaResponse|JsonResponse
     {
+        $book = $this->bookService->getBook($book);
         $bookDTO = BookDTO::fromModel($book);
 
         $additionalData = [
@@ -94,6 +95,7 @@ class BookController extends Controller
 
     public function show(Book $book): InertiaResponse|JsonResponse
     {
+        $book = $this->bookService->getBook($book);
         $bookDTO = BookDTO::fromModel($book);
 
         $additionalData = [
